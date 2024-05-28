833a084dd4458ba7c9554136cf03f38b8dd2d492
Matt Leslie <matt@mattleslie.dev>
2024-05-28 15:18:41 -0700

    1.1 updates

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,15 +1,41 @@
-const Hello = (props) => {
-  return (<div>
-    <p>Hello {props.name}</p>
-  </div>)
+const Header = (props) => {
+  return(
+    <h1>{props.course}</h1>
+  )
 }
-
-const App = () => {
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='Matt' />    </div>
+const Content = (props) => {
+  return(
+    <p>{props.part} {props.exercises}</p>
   )
 }
 
-export default App 
+const Total = (props) => {
+  return(
+    <p>
+      Number of exercises {props.sum}
+    </p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total sum={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
